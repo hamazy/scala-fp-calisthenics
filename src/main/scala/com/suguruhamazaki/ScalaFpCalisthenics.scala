947818899fd1a71260a442eb1457de0234160dd8
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-05-07 11:50:58 +0900

    Add examples for recursion and substitution model.

--- src/main/scala/com/suguruhamazaki/ScalaFpCalisthenics.scala
+++ src/main/scala/com/suguruhamazaki/ScalaFpCalisthenics.scala
@@ -4,4 +4,32 @@ object ScalaFpCalisthenics {
   def main(args: Array[String]): Unit = {
     println("Hello, World")
   }
+
+}
+
+object NaiveRecursion {
+  def isOdd(n: Long): Boolean =
+    if (n == 0) false
+    else isEven(n - 1)
+
+  def isEven(n: Long): Boolean =
+    if (n == 0) true
+    else isOdd(n - 1)
+}
+
+object MutualRecursion {
+  import scala.util.control.TailCalls.{ TailRec, done, tailcall }
+
+  def isOdd(n: Long): TailRec[Boolean] =
+    if (n == 0) done(false)
+    else tailcall(isEven(n - 1))
+
+  def isEven(n: Long): TailRec[Boolean] =
+    if (n == 0) done(true)
+    else tailcall(isOdd(n - 1))
+}
+
+object SubstitutionModel {
+  def sumOfSquares(i1: Int, i2: Int) = square(i1) + square(i2)
+  def square(i: Int) = i * i
 }
